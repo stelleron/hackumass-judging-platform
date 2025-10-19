c99e9d6f13a8e8c9a8ece00f68f4ebc1666cd39b
stelleron <shreyasdonti223@gmail.com>
2025-10-19 08:27:48 -0400

    Added title

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,11 +1,17 @@
 // pages/_app.tsx
+import Head from "next/head";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div>
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>HackUMass XIII Judging</title>
+      </Head>
+      <div>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
